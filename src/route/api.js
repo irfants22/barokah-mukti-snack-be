b2c9419302edbd24285855de7eb2256e216e9e61
irfants22 <irfanee02@gmail.com>
2025-06-22 04:50:54 +0700

    add api route for get all user api

--- src/route/api.js
+++ src/route/api.js
@@ -5,6 +5,8 @@ import { isAuthorized } from "../middleware/auth-middleware.js";
 const authorizedRouter = Router();
 authorizedRouter.use(isAuthorized);
 
+// User API
 authorizedRouter.get("/api/user/me", userController.getCurrentUser);
+authorizedRouter.get("/api/admin/user", userController.getAllUser);
 
 export { authorizedRouter };
