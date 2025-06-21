50e52317c0f6727656e8e5d51696f3362d6071ba
irfants22 <irfanee02@gmail.com>
2025-06-22 04:52:45 +0700

    add validation schema for get all user api

--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -12,4 +12,12 @@ const loginUserValidation = Joi.object({
   password: Joi.string().max(100).required(),
 });
 
-export { registerUserValidation, loginUserValidation };
+const getAllUserValidation = Joi.object({
+  query: Joi.string().optional(),
+  page: Joi.number().positive().min(1).default(1),
+  limit: Joi.number().max(10).default(10),
+  sortBy: Joi.string().optional(),
+  sortOrder: Joi.string().optional(),
+});
+
+export { registerUserValidation, loginUserValidation, getAllUserValidation };
