4b72228b81102077c0cccc6cf7fc0d215e538f73
irfants22 <irfanee02@gmail.com>
2025-06-22 10:47:35 +0700

    add api route for update current user api

--- src/route/api.js
+++ src/route/api.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import userController from "../controller/user-controller.js";
 import { isAuthorized } from "../middleware/auth-middleware.js";
+import upload from "../middleware/upload.js";
 
 const authorizedRouter = Router();
 authorizedRouter.use(isAuthorized);
 
-// User API
+// User API (user)
 authorizedRouter.get("/api/user/me", userController.getCurrentUser);
+authorizedRouter.put("/api/user/me", upload.single("image"), userController.updateCurrentProfile);
+
+// User API (admin)
 authorizedRouter.get("/api/admin/user", userController.getAllUser);
 
 export { authorizedRouter };
