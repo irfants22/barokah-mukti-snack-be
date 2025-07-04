3e42663dc69684abca133d5de39850b0a82d2732
irfants22 <irfanee02@gmail.com>
2025-07-04 18:13:03 +0700

    add sortorder to order validation schema

--- src/validation/order-validation.js
+++ src/validation/order-validation.js
@@ -8,6 +8,7 @@ const getAllOrderValidation = Joi.object({
   status: orderStatusValidation,
   page: Joi.number().positive().min(1).default(1),
   limit: Joi.number().max(10).default(10),
+  sortOrder: Joi.string().default("asc").optional(),
 });
 
 export { getAllOrderValidation, orderStatusValidation };
