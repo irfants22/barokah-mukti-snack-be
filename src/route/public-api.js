1263b2db7eca70f2702e25cbe9141e4f71b3208f
irfants22 <irfanee02@gmail.com>
2025-07-23 09:49:28 +0700

    add new route for product api and payment api

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import userController from "../controller/user-controller.js";
+import productController from "../controller/product-controller.js";
+import paymentController from "../controller/payment-controller.js";
 
 const publicRouter = Router();
 
@@ -7,7 +9,18 @@ publicRouter.get("/", (_req, res) => {
   res.status(200).send("Bagus, server telah berjalan");
 });
 
+// Auth API
 publicRouter.post("/api/auth/register", userController.register);
 publicRouter.post("/api/auth/login", userController.login);
+
+// Product API
+publicRouter.get("/api/products", productController.getAllProduct);
+publicRouter.get("/api/products/:productId", productController.getDetailProduct);
+
+// Payment API
+publicRouter.post("/api/payment/notification", paymentController.handlePaymentNotification);
+
+// Create Admin Alternative
+publicRouter.post("/api/create/admin", userController.createAdminUser);
 
 export { publicRouter };
